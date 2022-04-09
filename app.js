ceea7ac1fb806ed7022c423873b1db0449bf781a
unknown <nel.rsf@gmail.com>
2022-04-09 13:34:55 -0500

    deploy backend 1st time

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
+const https = require("https");
+const path  = require("path");
+const fs = require("fs");
+
+
+const port = 3000;
+
+mongoose.connect(process.env.DB_CONNECTION, 
+    ()=>{
+           console.log("connected to db");
+        }
+    )
+
+
+const apiProducts = require("./controller/getProducts");
+app.use("/getproducts", apiProducts)
+
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert','cert.pem'))}
+
+,app)
+
+
+sslServer.listen(port)
