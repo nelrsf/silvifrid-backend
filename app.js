4433f057de8e2ce69f8e9a739cb6d0a1a1046760
unknown <nel.rsf@gmail.com>
2022-04-09 14:52:38 -0500

    port fixed 2

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ app.use("/getproducts", apiProducts)
 
 const sslServer = https.createServer({
     key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert','cert.pem'))}
+    cert: fs.readFileSync(path.join(__dirname, 'cert','cert.pem'))
+}
 
 ,app)
 
 
-sslServer.listen(process.env.PORT||port)
+//sslServer.listen(process.env.PORT||port)
+app.listen(port)
