a96e1203f390fc8a86b90649ad9eaeaf5057b31f
unknown <nel.rsf@gmail.com>
2022-04-16 13:03:44 -0500

    Added assets endpoint

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ mongoose.connect(process.env.DB_CONNECTION,
 const apiProducts = require("./controller/getProducts");
 app.use("/getproducts", apiProducts)
 
+const apiAssets = require("./controller/getAssets");
+app.use("/getassets", apiAssets)
+
 const sslServer = https.createServer({
     key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
     cert: fs.readFileSync(path.join(__dirname, 'cert','cert.pem'))
