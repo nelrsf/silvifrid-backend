2cd9b7c8ebd2e1a8bb2a0e5b62488d82267a302d
unknown <nel.rsf@gmail.com>
2022-04-17 13:33:54 -0500

    server crashed

--- controller/socialMediaLogIn.js
+++ controller/socialMediaLogIn.js
@@ -26,7 +26,7 @@ router.get("/instagram", async (req, res)=>{
 
 router.get("/handleauth", async (req, res)=>{
     r_url = req.protocol + '://' + req.get('host') + req.originalUrl;
-    var req_url = new URL(r_url);
+    //var req_url = new URL(r_url);
 
     //res.send(req_url.searchParams.get('code'));
 })
