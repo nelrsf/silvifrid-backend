8aed560759e6254175d4ed4747cd39d818a30714
unknown <nel.rsf@gmail.com>
2022-04-17 13:55:17 -0500

    server down

--- controller/socialMediaLogIn.js
+++ controller/socialMediaLogIn.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const apiIG = require("instagram-node").instagram();
 const url = require('url');
 
-apiIG.use({ access_token: 'IGQVJYdXRmbFl2bFhsNDZAXSmJnVWZAtdzlOdGgtaUVmYUU1Ml95MHJFYXl1MnEzTnF4TWd5RlpKaWJlbWVpOWJBSW4wbllvQTFfa3BJWjlBM0ItS0RaUDc2c3NnUGh4UnBQTzZA3V25FS1RudWtIQ29rZAQZDZD' });
+
 apiIG.use({
     client_id: "1067590140841251",
     client_secret: "24673a3f0325b2492d62a4273588c270"
@@ -26,7 +26,7 @@ router.get("/instagram", async (req, res)=>{
 
 router.get("/handleauth", async (req, res)=>{
     r_url = req.protocol + '://' + req.get('host') + req.originalUrl;
-    var req_url = new URL(r_url);
+    var req_url = new url(r_url);
     res.send(req_url.searchParams.get('code'));
 })
 
