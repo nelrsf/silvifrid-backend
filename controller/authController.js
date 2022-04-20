895856431a3ad2be7deecfec4e53e1db407b47ed
unknown <nel.rsf@gmail.com>
2022-04-19 23:03:30 -0500

    instagram feature

--- controller/authController.js
+++ controller/authController.js
@@ -29,7 +29,7 @@ router.get("/", (req, res)=>{
 router.get("/handleauth", (req, res)=>{
 
   const tokenParams = {
-    tokenHost: "https://api.instagram.com",
+    tokenHost: "api.instagram.com",
     tokenPath: "/oauth/authorize",
     code: req.query.code,
     redirect_uri: 'https://silvifrid-server.herokuapp.com/login/handleauth',
