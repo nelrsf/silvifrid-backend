61e40f80e3abaa3cd373cf8a684c29fac8fff300
unknown <nel.rsf@gmail.com>
2022-04-20 07:46:48 -0500

    instagram feature

--- controller/authController.js
+++ controller/authController.js
@@ -26,12 +26,8 @@ router.get("/handleauth",function(req, res){
       console.log(err.body);
       res.send("error");
     }else{
-      //console.log("ok, token = ", result.access_token);
-      api.user('user_id', function(err, result, remaining, limit) {
-        console.log("error: ", err.error_message);
-        console.log(result);
-        res.send(err.body)
-      })
+      console.log("ok, token = ", result);
+
     }
   })
 })
